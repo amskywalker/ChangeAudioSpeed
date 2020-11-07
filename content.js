c20bed53e468348aaaf12c6481734843864d0d42
Adailton Moura <adailton.silva091@academico.ifs.edu.br>
2020-11-06 22:51:21 -0300

    Add speed options menu

--- content.js
+++ content.js
@@ -1,28 +1,57 @@
-    
-    const interval = setInterval(()=>{
+    const interval = setInterval(() => {
 
 
         const header = document.querySelector("._1QUKR");
         if (header) {
-            console.log(header)
             clearInterval(interval)
-            
-        
-            const button = document.createElement("button")
-            button.innerHTML = "2x"
-            button.classList.add("TimesButton")
-                
-            button.addEventListener("click", ()=>{
-                const audios = document.querySelectorAll("audio");
-                audios.forEach((audio) =>{
-                    audio.playbackRate = 2;
-                })
 
-            })
+            const div = document.createElement("div")
 
-            header.appendChild(button)  
+            div.classList.add("audioListOption")
+
+            const li = "<li class='speed1'>1x</li>" + "<li class='speed15'>1.5x</li>" + "<li class='speed2'>2x</li>";
+            const ul = "<ul class='listSpeedOptions'>" + li + "</ul>"
+            const text = "<h3 class='choosespeed'>Choose Speed</h3>" + ul
+            div.innerHTML = text;
+            const interval2 = setInterval(() => {
+                const button = document.querySelector(".choosespeed");
+                const list = document.querySelector(".listSpeedOptions");
+                if (button) {
+                    clearInterval(interval2)
+
+                    button.addEventListener("click", () => {
+                        if (list.style.display === "none") {
+                            list.style.display = "block";
+                        } else {
+                            list.style.display = "none";
+                        }
+                    })
+                    const speed1 = document.querySelector(".speed1");
+                    const speed15 = document.querySelector(".speed15");
+                    const speed2 = document.querySelector(".speed2");
+                    speed1.addEventListener("click", () => {
+                        const audios = document.querySelectorAll("audio");
+                        audios.forEach((audio) =>{
+                            audio.playbackRate = 1;
+                        })
+                    })
+                    speed15.addEventListener("click", () => {
+                        const audios = document.querySelectorAll("audio");
+                        audios.forEach((audio) =>{
+                            audio.playbackRate = 1.5;
+                        })
+                    })
+                    speed2.addEventListener("click", () => {
+                        const audios = document.querySelectorAll("audio");
+                        audios.forEach((audio) =>{
+                            audio.playbackRate = 2;
+                        })
+                    })
+                }
+            }, 500)
+
+            header.appendChild(div)
 
         }
 
-    },1000)
-   
+    }, 1000)
